6481879c47215b64e4606b233f4d71cb013eb826
DannyDov <dankodov@gmail.com>
2021-10-13 14:06:06 +0200

    Fixed Regression

--- regression.go
+++ regression.go
@@ -1,6 +1,9 @@
 package statistics
 
-import "math"
+import (
+	"errors"
+	"math"
+)
 
 func Sum(lst []float64) float64 {
 	var sum float64
@@ -12,58 +15,63 @@ func Sum(lst []float64) float64 {
 	return sum
 }
 
-func SquareSum(lst []float64) float64 {
-	var sum, sumSqr, square float64
+func SquareSum(arr []float64) float64 {
+	var (
+		sum       float64 = Sum(arr)
+		sumSqr    float64
+		squareSum float64
+	)
 
-	sum = Sum(lst)
-
-	for _, i := range lst {
+	for _, i := range arr {
 		sumSqr += i * i
 	}
 
-	square = sumSqr - (math.Pow(sum, 2) / float64(len(lst)))
+	squareSum = sumSqr - math.Pow(sum, 2)/float64(len(arr))
 
-	return square
+	return squareSum
 }
 
-func Correlation(X []float64, Y []float64) float64 {
+func Correlation(X []float64, Y []float64) (float64, error) {
+	if len(X) != len(Y) {
+		return 0, errors.New("Error: X and Y aren't equal")
+	}
+
 	var (
-		x, y, r float64
-		prodXY  float64
-		xy      []float64
+		Sxx, Syy, Sxy float64 = SquareSum(X), SquareSum(Y), 0
+		sumX, sumY    float64 = Sum(X), Sum(Y)
+		prodXY        []float64
 	)
 
 	for i := 0; i < len(X); i++ {
-		xy = append(xy, X[i], Y[i])
+		prodXY = append(prodXY, X[i]*Y[i])
 	}
 
-	x = SquareSum(X)
-	y = SquareSum(Y)
-	prodXY = SquareSum(xy)
+	Sxy = Sum(prodXY) - (sumX*sumY)/float64(len(X))
 
-	r = prodXY / math.Sqrt(x*y)
-
-	return r
+	return Sxy / math.Sqrt(Sxx*Syy), nil
 }
 
-func LinearRegression(X []float64, Y []float64) (float64, float64) {
+func LinearRegression(X []float64, Y []float64) (float64, float64, error) {
+	if len(X) != len(Y) {
+		return 0, 0, errors.New("X and Y aren't equal")
+	}
+
 	var (
-		x, y   float64
-		a, b   float64
-		prodXY float64
-		xy     []float64
+		Sxx, Sxy     float64 = SquareSum(X), 0
+		meanX, meanY float64 = Mean(X), Mean(Y)
+		sumX, sumY   float64 = Sum(X), Sum(Y)
+		grad, inter  float64
+		prodXY       []float64
 	)
 
 	for i := 0; i < len(X); i++ {
-		xy = append(xy, X[i], Y[i])
+		prodXY = append(prodXY, X[i]*Y[i])
 	}
 
-	prodXY = SquareSum(xy)
-	x = SquareSum(X)
-	y = SquareSum(Y)
+	Sxy = Sum(prodXY) - (sumX*sumY)/float64(len(X))
 
-	a = prodXY / math.Sqrt(x*y)
-	b = Mean(Y) - a*Mean(X)
+	grad = Sxy / Sxx
+	inter = meanY - meanX*grad
 
-	return a, b
+	return grad, inter, nil
 }
